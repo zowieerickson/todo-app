e7d40b3a27b4a6c9806c6cb76c42a3f73bf0a702
Zowie Erickson <zowieerickson@Zowies-MacBook-Air.local>
2025-01-12 11:30:16 -0500

    Add header background image, center header title position

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,14 @@ import React from "react";
 import styles from "./Header.module.css";
 
 const Header: React.FC = () => {
-    return <div>Hello, world!</div>
+    return (
+        <header className={styles.header}>
+            <div className={styles.title}>
+                <h1>TODO</h1>
+                <button className={styles.themeToggle} aria-label="Activate dark mode"></button>
+            </div>
+        </header>
+    )
 };
 
 export default Header;
