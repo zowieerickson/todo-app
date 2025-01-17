fee7318d0caa2291c3235952c068587bb01547b7
Zowie Erickson <zowie.erickson@gmail.com>
2025-01-17 14:04:47 -0500

    Bring AddTodo component to header and center

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Header from "./components/Header/Header"
-import Addtodo from "./components/AddTodo/AddTodo"
 import './App.css'
 
 function App() {
   return (
   <>
     <Header/>
-    <Addtodo/>
-
   </>
   )
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,19 @@
 import React, {useState} from "react";
 import styles from "./Header.module.css";
 import ButtonToggle from "../ButtonToggle/ButtonToggle"
+import AddTodo from "../AddTodo/AddTodo"
 
 const Header: React.FC = () => {
 
 
     return (
         <header className={styles.header}>
-            <div className={styles.title}>
-                <h1>TODO</h1>
-                <ButtonToggle />
+            <div className={styles.copyContainer}>
+                <div className={styles.title}>
+                    <h1>TODO</h1>
+                    <ButtonToggle />
+                </div>
+                <AddTodo />
             </div>
         </header>
     )
