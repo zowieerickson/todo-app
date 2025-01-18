694727ce47a4e3406f201675fd591f33de0faca0
Zowie Erickson <zowie.erickson@gmail.com>
2025-01-18 10:33:03 -0500

    Update text input styling and focus state

--- src/components/AddTodo/AddTodo.tsx
+++ src/components/AddTodo/AddTodo.tsx
@@ -1,11 +1,13 @@
 import React, {useState} from "react";
+import styles from "./AddTodo.module.css";
 
 const AddTodo: React.FC = () => {
 
 
     return (
         <form aria-label="Create a new todo">
-            <input 
+            <input
+                className={styles.addTodo}
                 type="text"
                 placeholder="Create a new todo..."
                 aria-label="Task description"
