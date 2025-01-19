0c13271ea66d1a655a49b8312c7a38b614f99f01
Zowie Erickson <zowie.erickson@gmail.com>
2025-01-19 10:39:32 -0500

    Prevent default on form when being submitted

--- src/components/AddTodo/AddTodo.tsx
+++ src/components/AddTodo/AddTodo.tsx
@@ -2,10 +2,13 @@ import React, {useState} from "react";
 import styles from "./AddTodo.module.css";
 
 const AddTodo: React.FC = () => {
-
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Submit being handled")
+    }
 
     return (
-        <form aria-label="Create a new todo">
+        <form onSubmit={handleSubmit} aria-label="Create a new todo">
             <input
                 className={styles.addTodo}
                 type="text"
