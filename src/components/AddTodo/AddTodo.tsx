7648a33c3aeee9dc25f7d433c9221f935b414afd
Zowie Erickson <zowie.erickson@gmail.com>
2025-01-20 08:08:38 -0500

    Render input values on page

--- src/components/AddTodo/AddTodo.tsx
+++ src/components/AddTodo/AddTodo.tsx
@@ -2,20 +2,32 @@ import React, {useState} from "react";
 import styles from "./AddTodo.module.css";
 
 const AddTodo: React.FC = () => {
+    const [todos, setTodos] = useState<string[]>([]);
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("Submit being handled")
+        const inputValue : string = e.target[0].value;
+        setTodos((prevTodos) => [...prevTodos, inputValue]) // Add inputValue to the todos list
+        console.log(inputValue, todos)
     }
 
     return (
-        <form onSubmit={handleSubmit} aria-label="Create a new todo">
-            <input
-                className={styles.addTodo}
-                type="text"
-                placeholder="Create a new todo..."
-                aria-label="Task description"
-            />
-        </form>
+        <>
+            <form onSubmit={handleSubmit} aria-label="Create a new todo">
+                <input
+                    className={styles.addTodo}
+                    type="text"
+                    placeholder="Create a new todo..."
+                    aria-label="Task description"
+                />
+            </form>
+
+            <ul className={styles.addTodo}>
+                {todos.map((todo, index) => (
+                   <li key={index}>{todo}</li>
+                ))}
+            </ul>
+        </>
     )
 };
 
