c4005b0df57822ca529d506c4ea69cef8a478efc
Zowie Erickson <zowie.erickson@gmail.com>
2025-01-29 10:19:56 -0500

    Bring new font JoseFin Sans from Google and change modern-normalize from being in the HTML to being imported

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'modern-normalize/modern-normalize.css'; // Import normalize first
 import './index.css'
 import App from './App.tsx'
 
